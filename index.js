bfe8bb8838a44db618a67664497de44a7dbd543f
ziggi <ziggi.willpower@gmail.com>
2021-03-18 14:34:03 +0100

    delete buttons work

--- index.js
+++ index.js
@@ -1,40 +1,133 @@
 "use strict";
 const library = document.getElementById("library");
+const addBookModal = document.getElementById("addBookModal");
+const openModalButton = document.getElementById("openModalButton");
+const closeModalButton = document.getElementById("closeModalButton");
+const addBookButton = document.getElementById("addBookButton");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const hasReadInput = document.getElementsByName("hasread");
 
 let myLibrary = [];
 let cardArray = [];
+
+closeModalButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    closeBookModal();
+})
+addBookButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    addBook(new Book(titleInput.value, authorInput.value, pagesInput.value, checkHasReadValue()))
+    closeBookModal();
+});
+
+addBookModal.addEventListener("click", (e) => {
+    if (e.target == addBookModal) {
+        closeBookModal();
+    }
+})
+
+openModalButton.addEventListener("click", () => addBookModal.style.display = "block")
+
+function checkHasReadValue() {
+    for (let i = 0; i < hasReadInput.length; i++) {
+        if (hasReadInput[i].checked) {
+            return hasReadInput[i];
+        }
+    }
+}
+
+function closeBookModal() {
+    addBookModal.style.display = "none";
+    clearForm();
+}
+
+function clearForm() {
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    for (let i = 0; i < hasReadInput.length; i++) {
+        hasReadInput[i].checked = false;
+    }
+}
 
 function Book(title, author, pages, hasRead) {
     this.title = title; 
     this.author = author;
     this.pages = pages;
-    this.hasRead = hasRead;
-    this.info = function() {
-        let read = (hasRead) ? "Have read" : "Have not read";
-        return title + 
-        " By: " + author + 
-        ". Pages: " + pages + " " + read;
-    } 
+    this.hasRead = hasRead ? "Have read" : "Have not read";
+    this.id = createId();
+}
+
+function createId() {
+  return Math.floor(Math.random() * (1000 - 100) + 100);
 }
 
 function addBook(book){
     myLibrary.push(book);
+    displayBook(book);
 }
 
 function createCardForBook(book){
     let newCard = document.createElement("div");
-    newCard.setAttribute("id", "card");
-    newCard.textContent = book.info();
+    newCard.setAttribute("class", "card");
+    newCard.setAttribute("id", book.id);
+    newCard.textContent = book.title + " by " + book.author + ", " + book.pages + " pages. " + book.hasRead;
     cardArray.push(newCard);
+    createDeleteButton(book);
+    return newCard;
 }
 
-function displayBooks(){
+function createReadStatusButton(book) {
+
+}
+
+function updateReadStatus(book) {
+
+}
+
+function createDeleteButton(book) {
+    let newButton = document.createElement("button");
+    newButton.setAttribute("class", "delete-button");
+    newButton.setAttribute("id", book.id);
+    newButton.textContent = "Delete book";
+    newButton.addEventListener("click", (e) => {
+        library.removeChild(findCard(findBook(e.target.id)));
+        myLibrary.splice(findBookIndex(e.target.id), 1);
+    });
+    appendElement(findCard(book), newButton)
+}
+
+function findCard(book) {
+    for (let i = 0; i < cardArray.length; i++) {
+        if (cardArray[i].id == book.id)
+          return cardArray[i]
+    }
+}
+
+function findBook(id) {
     for (let i = 0; i < myLibrary.length; i++) {
-        createCardForBook(myLibrary[i]);
+      if (myLibrary[i].id == id) {
+        return myLibrary[i];
+      }
     }
-    for (let i = 0; i < cardArray.length; i++){
-        library.appendChild(cardArray[i]);
+}
+
+function findBookIndex(id) {
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].id == id) {
+            return i
+        }
     }
+}
+ 
+function appendElement(parent, element) {
+    parent.appendChild(element);
+}
+
+function displayBook(book) {
+    appendElement(library, createCardForBook(book));
 }
 
 function initiateLibrary() {
@@ -45,5 +138,6 @@ function initiateLibrary() {
     addBook(new Book("The Memory Theatre", "Karin Tidbeck", 285, 0))
 }
 
+
 initiateLibrary();
-displayBooks();
+
